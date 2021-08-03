c046bce43b7b352facbafe5e12b837875ea9d331
madikarizma <madikarizma@gmail.com>
2021-08-03 12:35:41 -0400

    Update naming

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const githubLabelSync = require('github-label-sync');
 const config = require('./config.json');
 
-config.forEach((configElement) => {
-	configElement.repos.forEach((repo) => {
+config.forEach((configGroup) => {
+	configGroup.repos.forEach((repo) => {
 		githubLabelSync({
 			accessToken: process.env.GITHUB_ACCESS_TOKEN,
 			allowAddedLabels: false,
-			labels: configElement.labels,
+			labels: configGroup.labels,
 			repo
 		}).then((diff) => {
 			console.log(diff);
